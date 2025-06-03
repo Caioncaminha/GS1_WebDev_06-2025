718c4e72747e63dfa716fec7860545cd8e4a0324
1IMperaDOR0 <lucaslucore@gmail.com>
2025-06-02 22:20:27 -0300

    Finishing the themes

--- src/js/script.js
+++ src/js/script.js
@@ -9,23 +9,40 @@ menu.addEventListener('click', () => {
 menuClose.addEventListener('click', () => {
     nav.style.display = 'none'
 })
+
 function setTheme(mode) {
+    let bgColorH = '#008d8d';
+
+    let bgColorF = '#008d8d';
+    
     let bgColor = "#fff";
-    let textColor = "#000";
+    let textColor = "#000"; 
 
     if (mode === "dark") {
+        bgColorH = '#005c5c';
+        
+        bgColorF = '#005c5c';
+
         bgColor = "#111";
-        textColor = "#eee";
+        textColor = "#fff";
     } else if (mode === "blue") {
+        bgColorH = '#036e8f';
+        
+        bgColorF = '#036e8f';
+
         bgColor = "#e0f7fa";
         textColor = "#004d40";
     }
+
+    document.querySelector('header').style.backgroundColor = bgColorH;
+
+    document.querySelector('footer').style.backgroundColor = bgColorF;
 
     document.body.style.backgroundColor = bgColor;
     document.body.style.color = textColor;
 
     // Atualiza elementos interativos
-    const allElements = document.querySelectorAll("nav, footer, section");
+    const allElements = document.querySelectorAll("section");
     allElements.forEach(el => {
         el.style.backgroundColor = bgColor;
         el.style.color = textColor;
